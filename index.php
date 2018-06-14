aa514e861257432f3dc4d1249050cbb7479aee6c
Misiurak <pmisiura@bbyfs.ca>
2018-06-14 14:26:41 -0700

    another change, jenkins should deploy

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <title>PHP test</title>
   </head>
   <body>
-  <?php echo '<h1>an example application</h1>'; ?>
+  <?php echo '<h1>an example application</h1><br/><p>Jenkins should deploy this change</p>'; ?>
   </body>
 </html>
